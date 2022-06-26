1acefd68013dfcaf137c001c8b54ac23edb77818
bgradecoding <iinee12@kakao.com>
2022-06-26 13:50:54 +0900

    log add

--- controller/myplaylist.js
+++ controller/myplaylist.js
@@ -4,10 +4,19 @@ import * as myplaylistModel from "../model/myplaylist.js";
 
 export async function createPlayList(req, res) {
   const userSub = util.getUserSubFormToken(req);
-  //const userSub = "dsfsd";
+
   const { playlistTitle, playlistDesc } = req.body;
+
   const playlistId =
     userSub + new Date().getTime() + Math.floor(Math.random() * 100);
+
+  console.log(
+    "client values----",
+    userSub,
+    playlistTitle,
+    playlistDesc,
+    playlistId
+  );
 
   const newPlaylistId = await myplaylistModel.createPlayList(
     playlistId,
