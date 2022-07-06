ca462e569b82a24952d446ba9e1c0731a5b42756
bgradecoding <iinee12@kakao.com>
2022-07-06 13:34:54 +0900

    validator add for create playlist

--- routes/myplaylist.js
+++ routes/myplaylist.js
@@ -1,15 +1,33 @@
 import express from "express";
 import * as myplaylistController from "../controller/myplaylist.js";
 import * as util from "../util/util.js";
+import { body } from "express-validator";
 
 const router = express.Router();
+
+const validateCreatePlaylist = [
+  body("playlistTitle")
+    .notEmpty()
+    .withMessage("플레이리스트 제목을 입력해야합니다."),
+  body("playlistDesc")
+    .notEmpty()
+    .withMessage("플레이리스트에 대한 설명이 필요합니다."),
+  body("tags")
+    .isArray()
+    .notEmpty()
+    .withMessage("하나 이상의 태그를 선택해야 합니다."),
+];
 
 //token 유효성 검사
 router.use((req, res, next) => {
   util.authenticateToken(req, res, next);
 });
 
-router.post("/playlist", myplaylistController.createPlayList);
+router.post(
+  "/playlist",
+  validateCreatePlaylist,
+  myplaylistController.createPlayList
+);
 router.post("/movie", myplaylistController.createPlMovie);
 router.get("/playlist", myplaylistController.getPlayList);
 router.get("/tag", myplaylistController.getTags);
